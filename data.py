73fc72720e4695436e5472715f9b89c525efb119
Will Howard <willh0455@gmail.com>
2025-10-12 10:21:12 -0500

    Restore helpers.py and data.py from Sprint 7 base

---
+++ data.py
@@ -0,0 +1,12 @@
+# This file stores test data constants for Project 7
+
+# URL of the Urban Routes app (for now, empty string until we start the server)
+URBAN_ROUTES_URL = 'https://cnt-5b691a28-1d7c-4ff2-a427-4a2c423006b1.containerhub.tripleten-services.com/'
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
